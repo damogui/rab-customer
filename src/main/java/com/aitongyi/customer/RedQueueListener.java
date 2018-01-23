a7bcb82494f467358b14d96e4db3b2ef4704172d
gj <jjguo@gongsibao.com>
2018-01-23 20:16:23 +0800

    Initial commit

---
+++ src/main/java/com/aitongyi/customer/RedQueueListener.java
@@ -0,0 +1,25 @@
+/**
+ * TODO
+ * 
+ */
+package com.aitongyi.customer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author hushuang
+ *
+ */
+public class RedQueueListener{
+	private static Logger log = LoggerFactory.getLogger(RedQueueListener.class);
+	/**
+	 * 处理消息
+	 * @param message
+	 * void
+	 */
+	public void onMessage(String message) {
+		log.info("RedQueueListener Receved:"  + message);
+        System.out.println("红频道收到消息:"+message);
+	}
+}
